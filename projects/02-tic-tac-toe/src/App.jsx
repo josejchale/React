32a9c9ada9ac8b8ea49cf1f4fc5c1a8107fb5f33
Jose Chale <chalejosejuan@gmail.com>
2024-05-29 18:23:13 -0600

    detectar ganador

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -19,19 +19,58 @@ const handleClick=()=>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8]
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App(){
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
 const [turn, setTurn] =useState(TURNS.X)
 
+const [winner, setWinner] = useState(null)
+
+const CheckWinner = (boardToCheck) => {
+//revisar las combinaciones ganadores
+//para saber si hay un ganador
+  for (const combo of WINNER_COMBOS){
+    const [a,b,c] = combo
+    if(
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b]&&
+      boardToCheck[a] === boardToCheck[c]
+    ){
+      return boardToCheck[a]
+    }
+  }
+//si no hay ganador
+  return null
+}
+
 const updateBoard=(index)=>{
-  const newBoard = [...board]
+// no se actualiza
+//si ya tiene algo
+if(board[index] || winner) return
+//actualizar el tablero
+  const newBoard = [ ... board]
   newBoard[index] = turn
   setBoard(newBoard)
-
+//cambiar el turno
 const newTurn = turn === TURNS.X ? TURNS.O :TURNS.X
 setTurn(newTurn)
+//revisar si hay ganador
+const newWinner = CheckWinner(newBoard)
+if (newWinner){
+  setWinner(newWinner)
+ }
 }
 
   return (
